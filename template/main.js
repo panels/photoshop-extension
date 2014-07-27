8d8129fb1dd2a0e6ae63baddaff6e54ebec2604c
marekhrabe <marekhrabe@abdoc.net>
2014-07-28 01:22:14 +0200

    making it work for non-auth extensions

--- template/main.js
+++ template/main.js
@@ -5,11 +5,36 @@ var pluginAuthId = '<%= panel['plugin-auth-id'] %>'
 var authToken = null;
 <% } else  {%>
 var requiresAuth = false;
+var pluginAuthId = null
+var authToken = null
 <% } %>
 
+var iframe = null
+
+var poll = function () {
+  var script = document.createElement('script')
+  script.src = 'http://127.0.0.1:<%= panel.port %>/_panels/ping?callback=init&random=' + Math.random()
+  script.onerror = function () {
+    document.body.removeChild(script)
+    delete script
+    setTimeout(poll, 1000)
+  }
+  document.body.appendChild(script)
+}
+
+window.changeTheme = function(e) {
+  var skinInfo = JSON.parse(window.__adobe_cep__.getHostEnvironment()).appSkinInfo;
+  var themeObject = {
+    type:'themeChange',
+    theme:'dark',
+    skinInfo: skinInfo
+  }
+
+  iframe.contentWindow.postMessage(themeObject, '*')
+}
+
 var checkAuth = function() {
-  var iframe  = document.getElementById('app')
-  if(requiresAuth) {
+  if (requiresAuth) {
     document.body.classList.add('login')
     window.addEventListener("message", function(event) {
     if(event.data && event.data.type === 'authenticationResult') {
@@ -31,42 +56,24 @@ var checkAuth = function() {
   }
 }
 
-var poll = function () {
-  var script = document.createElement('script')
-  script.src = 'http://127.0.0.1:37379/_panels/ping?callback=init&random=' + Math.random()
-  script.onerror = function () {
-    document.body.removeChild(script)
-    delete script
-    setTimeout(poll, 1000)
-  }
-  document.body.appendChild(script)
-}
-
-window.changeTheme = function(e) {
-
-  var iframe = document.getElementById('app')
-  var skinInfo = JSON.parse(window.__adobe_cep__.getHostEnvironment()).appSkinInfo;
-  var themeObject = {
-    type:'themeChange',
-    theme:'dark',
-    skinInfo: skinInfo
-  }
-
-  iframe.contentWindow.postMessage(themeObject, '*')
-}
-
 window.init = function () {
   if (bootstraped) {
     return
   }
   bootstraped = true
 
-  var iframe = document.getElementById('app')
+  iframe = document.getElementById('app')
   iframe.onload = function () {
-    window.__adobe_cep__.addEventListener("com.adobe.csxs.events.ThemeColorChanged", changeTheme)
+    window.__adobe_cep__ && window.__adobe_cep__.addEventListener("com.adobe.csxs.events.ThemeColorChanged", changeTheme)
+    document.body.classList.add('loaded')
     changeTheme()
   }
-  iframe.src = 'http://127.0.0.1:37379/?platform=photoshop&debug' + ((requiresAuth === true) ? '&token=' + authToken : '') + ((pluginAuthId) ? '&pluginAuthId=' + pluginAuthId : '')
+
+  if (requiresAuth) {
+    checkAuth()
+  } else {
+    iframe.src = 'http://127.0.0.1:<%= panel.port %>/?platform=photoshop&debug' + ((requiresAuth === true) ? '&token=' + authToken : '') + ((pluginAuthId) ? '&pluginAuthId=' + pluginAuthId : '')
+  }
 }
 
-window.onload = checkAuth
+window.onload = poll
